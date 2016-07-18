b30b5848df38ba1fd4486824c8ad69f404ad13ce
Juan Granda <juanfromecuador@gmail.com>
2016-07-18 08:03:19 -0400

    pick up objects and points, jump script

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class playerScript : MonoBehaviour {
 
 	private Rigidbody rb;
+	private int count;
 	public float speed;
+	public float jump;
+	public Text countText;
+	public Text winText;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb.AddForce (new Vector3 (0, jump, 0));
+		}
 	}
 	
 	// Update is called once per frame
@@ -18,5 +33,20 @@ public class playerScript : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
 		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count:" + count.ToString ();
+		if (count >= 24) {
+			winText.text = "You Win!!";
+		}
 	}
 }
