5714a1f2aba67b71d35a2e1e0eefcdb1faed3ebf
Juan Granda <juanfromecuador@gmail.com>
2016-07-17 23:08:35 -0400

    First commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+
+		//Camera.mainCamera.transform.eulerAngles = Vector3.Lerp(Camera.mainCamera.transform.eulerAngles, new Vector3(90, 0, 0), Time.deltaTime*speed);
+	}
+}
